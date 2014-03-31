6cf0958c78385e79f5e57a3279980eba2f608ad1
Nickolaus Saint <nick@incendiarysoftware.com>
2014-03-31 09:52:08 -0700

    Added editor types
    These will allow for the inspector to change values via properties,
    instead of directly through public variables.

---
+++ Unity Free Flight/Assets/Editor/MyTypeEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor( typeof( MyType ) )]
+public class MyTypeEditor : Editor {
+	
+	
+	MyType m_Instance;
+	PropertyField[] m_fields;
+	
+	
+	public void OnEnable()
+	{
+		m_Instance = target as MyType;
+		m_fields = ExposeProperties.GetProperties( m_Instance );
+	}
+	
+	public override void OnInspectorGUI () {
+		
+		if ( m_Instance == null )
+			return;
+		
+		this.DrawDefaultInspector();
+		
+		ExposeProperties.Expose( m_fields );
+		
+	}
+}
---
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/model/ExposePropertyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[AttributeUsage( AttributeTargets.Property )]
+public class ExposePropertyAttribute : Attribute
+{
+	
+}
