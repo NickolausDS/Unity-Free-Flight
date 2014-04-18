c31b9defa7ec9d6e3150318b89a2e936152dbb40
Nickolaus Saint <nick@incendiarysoftware.com>
2014-04-18 10:11:42 -0700

    Scene changes to add various script components

--- Unity Free Flight/Assets/UnityFreeFlight/demo/PauseMenu.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/demo/PauseMenu.cs
@@ -13,7 +13,6 @@ public class PauseMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
-			Debug.Log ("Paused!");
 			pause ();
 		}
 	
