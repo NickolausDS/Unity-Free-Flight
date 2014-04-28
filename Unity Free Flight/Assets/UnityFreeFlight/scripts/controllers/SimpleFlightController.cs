c8540202011eeaafff93d8f1074201a35d47559a
Nickolaus Saint <nick@incendiarysoftware.com>
2014-04-28 08:32:40 -0700

    Added simple flapping, which can be enabled via the flight controller

--- Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/SimpleFlightController.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/SimpleFlightController.cs
@@ -4,7 +4,15 @@ using System.Collections;
 public class SimpleFlightController : BaseFlightController {
 
 	Vector3 keyInput;
-	
+
+	public bool flappingEnabled = false;
+	public float flaptime = 0.5f;
+	public float flapStrength = 20.0f;
+
+	//Private flapping vars
+	private bool flapping;
+	private bool wingsHaveFlappedInDownPosition;
+	private float currentFlaptime;
 
 	void Update() {
 		//Pitch
@@ -15,6 +23,29 @@ public class SimpleFlightController : BaseFlightController {
 		//	keyInput.y = Input.GetAxis("Yaw") * (_rotationSpeed * Time.deltaTime);
 		_userInput.eulerAngles = keyInput;
 
+		if (flappingEnabled) {
+			flapWings ();
+		}
+
 	}
+
+	void flapWings() {
+
+		if (flapping) {
+			currentFlaptime += Time.deltaTime;
+			if (currentFlaptime > flaptime * 0.9f && !wingsHaveFlappedInDownPosition) {
+				rigidbody.AddForce( new Vector3 (0, -flapStrength/2, 0));
+				wingsHaveFlappedInDownPosition = true;
+			} else if (currentFlaptime > flaptime) {
+				flapping = false;
+				wingsHaveFlappedInDownPosition = false;
+			}
+		} else if (Input.GetButton("Jump") && !flapping) {
+			flapping = true;
+			currentFlaptime = 0.0f;
+			rigidbody.AddForce (new Vector3 (0, flapStrength, 0));
+		}
+	}
+
 
 }
