95acfe04174435c100e0f2c552cdd010f01bd598
Nickolaus Saint <nick@incendiarysoftware.com>
2014-04-29 09:50:07 -0700

    Moved flapping mechanic to base flight controller for simplicity

--- Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/BaseFlightController.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/BaseFlightController.cs
@@ -13,6 +13,16 @@ public class BaseFlightController : MonoBehaviour {
 	public Quaternion UserInput { get { return _userInput; } }
 
 
+	public bool flappingEnabled = false;
+	public float regularFlaptime = 0.7f;
+	public float minimumFlapTime = 0.2f;
+	public float flapStrength = 400.0f;
+	
+	//Private flapping vars
+	private bool flapping;
+	private bool wingsHaveFlappedInDownPosition;
+	private float currentFlaptime;
+
 	public bool Inverted {
 		get {
 			if (_invertedSetting == 1) 
@@ -26,6 +36,7 @@ public class BaseFlightController : MonoBehaviour {
 				_invertedSetting = 1;
 		}
 	}
+	
 
 	void Update() {
 		if (!_hasWarnedUser) {
@@ -34,6 +45,36 @@ public class BaseFlightController : MonoBehaviour {
 //			MonoBehaviour thisobj = gameObject.GetComponent<BaseFlightController>();
 //			thisobj.SendMessage("setDefaultController", SendMessageOptions.DontRequireReceiver);
 		}
+	}
+
+	public void flapWings(bool interruptFlap = false) {
+
+		if (flappingEnabled) {
+
+			//We can only flap if we're not currently flapping, or the user triggered
+			//an 'interruptFlap', which just means "We're flapping faster than the regular
+			//flap speed." InterruptFlaps are usually triggered by the user mashing the flap
+			//button rather than just holding it down.
+			if (!flapping || (interruptFlap && currentFlaptime > minimumFlapTime)) {
+				flapping = true;
+				currentFlaptime = 0.0f;
+				rigidbody.AddForce (new Vector3 (0, flapStrength, 0));
+			}
+
+			//Here we deal with flapping at a regular interval. It may be better to use something
+			//other than time.deltatime, it may give us incorrect readings
+			if (flapping) {
+				currentFlaptime += Time.deltaTime;
+				if (currentFlaptime > regularFlaptime * 0.9f && !wingsHaveFlappedInDownPosition) {
+					rigidbody.AddForce( new Vector3 (0, -flapStrength/4, 0));
+					wingsHaveFlappedInDownPosition = true;
+				} else if (currentFlaptime > regularFlaptime) {
+					flapping = false;
+					wingsHaveFlappedInDownPosition = false;
+				}
+			}
+		}
+
 	}
 
 	// Use this for initialization
--- Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/SimpleFlightController.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/SimpleFlightController.cs
@@ -5,14 +5,6 @@ public class SimpleFlightController : BaseFlightController {
 
 	Vector3 keyInput;
 
-	public bool flappingEnabled = false;
-	public float flaptime = 0.5f;
-	public float flapStrength = 20.0f;
-
-	//Private flapping vars
-	private bool flapping;
-	private bool wingsHaveFlappedInDownPosition;
-	private float currentFlaptime;
 
 	void Update() {
 		//Pitch
@@ -23,28 +15,12 @@ public class SimpleFlightController : BaseFlightController {
 		//	keyInput.y = Input.GetAxis("Yaw") * (_rotationSpeed * Time.deltaTime);
 		_userInput.eulerAngles = keyInput;
 
-		if (flappingEnabled) {
+		if (Input.GetButtonDown("Jump") ) {
+			flapWings (true);
+		} else if (Input.GetButton ("Jump")) {
 			flapWings ();
 		}
 
-	}
-
-	void flapWings() {
-
-		if (flapping) {
-			currentFlaptime += Time.deltaTime;
-			if (currentFlaptime > flaptime * 0.9f && !wingsHaveFlappedInDownPosition) {
-				rigidbody.AddForce( new Vector3 (0, -flapStrength/2, 0));
-				wingsHaveFlappedInDownPosition = true;
-			} else if (currentFlaptime > flaptime) {
-				flapping = false;
-				wingsHaveFlappedInDownPosition = false;
-			}
-		} else if (Input.GetButton("Jump") && !flapping) {
-			flapping = true;
-			currentFlaptime = 0.0f;
-			rigidbody.AddForce (new Vector3 (0, flapStrength, 0));
-		}
 	}
 
 
