e4ba4b941b7a1bd1c5b0308495419b3b719f54a9
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-06 10:20:01 -0700

    Made unitConverter more simple and completely extendable

--- Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/UnitConverter.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/UnitConverter.cs
@@ -6,16 +6,69 @@ using System;
 public class UnitConverter {
 
 	public enum Units{ Metric, Imperial };
-	public enum Types { Length, Area, Weight };
-//	public enum Imperial { Feet, SqFeet, Pounds };
-//	public enum Metric {Meters, SqMeters, Kilograms };
+	public enum Types { Length, Area, Weight, Force };
 
-	public static string[] imperial = {"FT", "FT^2", "LB"};
-	public static string[] metric = {"M", "M^2", "KG"};
+	public static string[] metric = {"M", "M^2", "KG", "N"};
+	public static string[] imperial = {"FT", "FT^2", "LB", "LBf"};
 
-	private static float METSQ2FTSQ = 10.764f;
-	private static float MET2FT = 3.28084f;
-	private static float KG2LB = 2.20462f;
+	private static float METSQ2FTSQ = 	10.7640f;
+	private static float MET2FT = 		3.28084f;
+	private static float KG2LB = 		2.20462f;
+	private static float N2LBF = 		0.22481f;
+	
+	static float[,] formulas = new float[,] {
+		//We store everything in metric, so any metric conversions are always 1.
+		{1.0f, 1.0f, 1.0f},
+		//meters to feet, sqm to sqft, kg to lb, Newtons to lbForce 
+		{ 3.28084f, 10.7640f, 2.20462f, 0.22481f }
+	};
+
+	private delegate float linearConv (float value, float formula);
+	static linearConv[,] getConversions = new linearConv[,] { 
+		{ reflexive, reflexive, reflexive, reflexive },		
+		{ linearGetConversion, linearGetConversion, linearGetConversion, linearGetConversion }
+	};
+
+	static linearConv[,] setConversions = new linearConv[,] { 
+		{ reflexive, reflexive, reflexive, reflexive },		
+		{ linearSetConversion, linearSetConversion, linearSetConversion, linearSetConversion }
+	};
+
+	public static float getConv(Units theUnit, Types theType, float theValue) {
+		float theFormula = formulas [(int) theUnit, (int) theType];
+		return getConversions 		[(int) theUnit, (int) theType] (theValue, theFormula);
+	}
+
+	public static float setConv(Units theUnit, Types theType, float theValue) {
+		float theFormula = formulas [(int) theUnit, (int) theType];
+		return setConversions 		[(int) theUnit, (int) theType] (theValue, theFormula);
+	}
+
+	public static float convert(Units fromUnit, Units toUnit, Types theType, float theValue) {
+		float val = theValue;
+		if (fromUnit != Units.Metric)
+			val = setConv(fromUnit, theType, theValue);
+		return getConv (toUnit, theType, theValue);
+	}
+
+	static float linearGetConversion (float value, float formula) {
+		return value * formula;
+	}
+	
+	static float linearSetConversion (float value, float formula) {
+		return value / formula;
+	}
+	
+	private static float reflexive (float v, float f) {
+		return v;
+	}
+
+
+//	private static float linearConversion(float value, float formula) {
+//		if (_unit == Units.Metric) {
+//
+//
+//	}
 
 
 	public Units _unit = Units.Metric;
