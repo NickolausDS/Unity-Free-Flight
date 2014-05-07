873b44ca3fa91384e22c756e81f94e0addc003ab
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-07 08:22:19 -0700

    More updates to Unit converter
    Force now completely valid unit
    Refactored how names are retrieved from the unit converter
    Added type names, in addition to their abreviations

--- Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/UnitConverter.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/UnitConverter.cs
@@ -8,11 +8,21 @@ public class UnitConverter {
 	public enum Units{ Metric, Imperial };
 	public enum Types { Length, Area, Weight, Force };
 
-	public static string[] metric = {"M", "M^2", "KG", "N"};
-	public static string[] imperial = {"FT", "FT^2", "LB", "LBf"};
+	public static string[,] names = {
+		{"Meters", "Square Meters", "Kilograms", "Newtons"},
+		{"Feet", "Square Feet", "Pounds", "Force Pounds"}
+	};
+
+	public static string[,] abreviations = {
+		{"M", "M^2", "KG", "N"},
+		{"FT", "FT^2", "LB", "LBf"}
+	};
+
+//	public static string[] metric = {"M", "M^2", "KG", "N"};
+//	public static string[] imperial = {"FT", "FT^2", "LB", "LBf"};
 	static float[,] formulas = new float[,] {
 		//We store everything in metric, so any metric conversions are always 1.
-		{1.0f, 1.0f, 1.0f},
+		{1.0f, 1.0f, 1.0f, 1.0f},
 		//meters to feet, sqm to sqft, kg to lb, Newtons to lbForce 
 		{ 3.28084f, 10.7640f, 2.20462f, 0.22481f }
 	};
@@ -65,24 +75,30 @@ public class UnitConverter {
 
 	public UnitConverter() {_unit = Units.Metric;}
 
-	public string getLengthType() {
-		return getUnitType (Types.Length);
-	}
-	public string getAreaType() {
-		return getUnitType (Types.Area);
-	}
-	public string getWeightType() {
-		return getUnitType (Types.Weight);
+	public string getTypeName(Units theUnit, Types theType, bool abrev=true){
+		if (abrev)
+			return abreviations [(int) theUnit, (int) theType];
+		return names[(int) theUnit, (int) theType];
 	}
 
-	public string getUnitType(Types thetype) {
-			if (_unit == Units.Metric) {
-						return metric [(int)thetype];
-				} else if (_unit == Units.Imperial) {
-						return imperial [(int)thetype];
-				} else {
-						throw new Exception ("Tried to get unit type that doesn't exist");
-				}
+	public string getTypeName(Types thetype, bool abrev=true) {
+		return getTypeName (_unit, thetype, abrev);
+	}
+
+	public string getLengthType(bool abrev=true) {
+		return getTypeName (Types.Length, abrev);
+	}
+
+	public string getAreaType(bool abrev=true) {
+		return getTypeName (Types.Area, abrev);
+	}
+
+	public string getWeightType(bool abrev=true) {
+		return getTypeName (Types.Weight, abrev);
+	}
+
+	public string getForceType(bool abrev=true) {
+		return getTypeName (Types.Force, abrev);
 	}
 
 }
