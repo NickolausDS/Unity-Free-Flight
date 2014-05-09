ca9a4bf00ce19564b29fafbf086fe8cdbc4f575f
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-09 08:45:24 -0700

    Added InfoMenu for basic user instruction on first play

---
+++ Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoMenu : MonoBehaviour {
+
+	void OnGUI() {
+		
+		string message = "'Esc' to pause.";
+		
+		GUI.BeginGroup (new Rect (Screen.width - 130, 10, 200, 40));
+		GUI.Box (new Rect (0, 0, 120, 25), message);
+		GUI.EndGroup ();
+	}
+}
