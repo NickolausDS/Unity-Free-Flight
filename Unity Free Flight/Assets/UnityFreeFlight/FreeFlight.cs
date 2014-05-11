be4038471bfd2a965dfa4a7b5095e7d5265e0070
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-11 09:48:41 -0700

    Added a state machine for toggling ground or flight modes

--- Unity Free Flight/Assets/UnityFreeFlight/FreeFlight.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/FreeFlight.cs
@@ -4,10 +4,16 @@ using System.Collections;
 [RequireComponent (typeof(Rigidbody))]
 public class FreeFlight : MonoBehaviour {
 
-	
+	public enum Modes {None, Ground, Flight, Hybrid};
+	/*
+	 * None -- Set no controller active, no flight physics active
+	 * Ground -- Set only ground controller active, no flight physics active
+	 * Flight -- Set only flight controller active, flight physics active
+	 * Hybrid -- Flight controller disabled, ground controller active, flight physics active
+	 */ 
 	public BaseFlightController flightController = null;
 	public MonoBehaviour groundController = null;
-	private bool _groundMode = false;
+	private Modes _mode = Modes.Flight;
 	private FlightPhysics _physicsObject;
 
 		
@@ -29,57 +35,116 @@ public class FreeFlight : MonoBehaviour {
 			} 
 		}
 
-	public bool GroundMode {
-		get {return _groundMode;}
+	//Tries to swap to the vairous controller modes.
+	//Success if controllers are set up properly, otherwise
+	//fails with an error to console and reverts mode to None
+	public Modes Mode {
+		get {return _mode;}
 		set {
-			if(_groundMode == value)
+			if(_mode == value)
 				return;
-			_groundMode = value;
-			if (_groundMode)
-				switchToGround();
-			else
-				switchToFlight();
+			_mode = value;
+			if (_mode == Modes.None) {
+				disableFlight();
+				disableGround();
+			} else if (_mode == Modes.Flight) {
+				disableGround();
+				if (!enableFlight()) {
+					_mode = Modes.None;
+				}
+			} else if (_mode == Modes.Ground || _mode == Modes.Hybrid) {
+				disableFlight ();
+				if (!enableGround()) {
+					_mode = Modes.None;
+				}
+			}
 		}
 	}
 		
 	void FixedUpdate() {
 
-		if (!_groundMode) {
+		if (_mode == Modes.Flight || _mode == Modes.Hybrid) {
 			PhysicsObject.doStandardPhysics(flightController.UserInput);
 		}
 	}
 
-	private void switchToGround() {
+	private bool disableGround() {
+		CharacterController cc = gameObject.GetComponent<CharacterController> ();
+		if (cc) {
+			cc.enabled = false;
+		}
+		if (groundController) {
+			groundController.enabled = false;		
+			return true;
+		}
+		return false;
+	}
 
+	private bool enableGround() {
 		if (groundController) {
 			groundController.enabled = true;
-			flightController.flightEnabled = false;
 			CharacterController cc = gameObject.GetComponent<CharacterController> ();
 			if (cc) {
 				cc.enabled = true;
 			}
-		} else {
-			Debug.Log ("No ground controller detected, not switching to ground controls");
-			GroundMode = !GroundMode;
+			return true;
 		}
-
+		Debug.LogError ("Failed to switch " + gameObject.name + " to ground controller. " +
+		                "Make sure you have a valid ground controller attached (The generic Character" +
+		                " Controller scripts will work as ground controllers).");
+		return false;
 	}
 
-	private void switchToFlight() {
+	private bool disableFlight() {
+		if (flightController) {
+			flightController.flightEnabled = false;
+			return true;
+		}
+		return false;
+	}
+
+	private bool enableFlight() {
 		if (flightController) {
 			flightController.flightEnabled = true;
-			if (groundController) {
-				groundController.enabled = false;
-			}
-			CharacterController cc = gameObject.GetComponent<CharacterController> ();
-			if (cc) {
-				cc.enabled = false;
-			}
-		} else {
-			Debug.Log ("No air controller detected, not switching to air controls");
-			GroundMode = !GroundMode;
+			return true;
 		}
+		Debug.LogError ("Failed to switch " + gameObject.name + " to flight controller. " +
+		                "Make sure you have a valid flight controller attached. You can find" +
+		                " Flight Controllers under FreeFlight/Scripts/Controllers.");
+		return false;
 	}
+
+//	private void switchToGround() {
+//
+//		if (groundController) {
+//			groundController.enabled = true;
+//			flightController.flightEnabled = false;
+//			CharacterController cc = gameObject.GetComponent<CharacterController> ();
+//			if (cc) {
+//				cc.enabled = true;
+//			}
+//		} else {
+//			Debug.Log ("No ground controller detected, not switching to ground controls");
+//			GroundMode = !GroundMode;
+//		}
+//
+//	}
+//
+//	private void switchToFlight() {
+//		if (flightController) {
+//			flightController.flightEnabled = true;
+//			if (groundController) {
+//				groundController.enabled = false;
+//			}
+//			CharacterController cc = gameObject.GetComponent<CharacterController> ();
+//			if (cc) {
+//				cc.enabled = false;
+//			}
+//		} else {
+//			Debug.Log ("No air controller detected, not switching to air controls");
+//			GroundMode = !GroundMode;
+//		}
+//	}
 
 
 	
