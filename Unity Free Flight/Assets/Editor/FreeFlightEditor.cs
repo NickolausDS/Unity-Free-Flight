69b5c2443c8d0a52bb0314cc38e230cb68b58523
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-12 08:47:23 -0700

    Added foldouts to FreeFlight editor

--- Unity Free Flight/Assets/Editor/FreeFlightEditor.cs
+++ Unity Free Flight/Assets/Editor/FreeFlightEditor.cs
@@ -6,8 +6,13 @@ using UnityEditor;
 public class FreeFlightEditor : Editor {
 
 	MonoBehaviour groundController = null;
+
 	MonoScript tempgc;
 	string cname = null;
+
+	private bool _showPhysicsAttrs = false;
+	private bool _showDimensionAttrs = false;
+	private bool _showControllers = true;
 
 
 	public override void OnInspectorGUI() {
@@ -15,25 +20,40 @@ public class FreeFlightEditor : Editor {
 		FlightPhysics fo = ff.PhysicsObject;
 		GameObject go = ff.gameObject;
 
-		fo.liftEnabled = EditorGUILayout.Toggle ("Lift", fo.liftEnabled);
-		fo.dragEnabled = EditorGUILayout.Toggle ("Drag", fo.dragEnabled);
-		fo.gravityEnabled = EditorGUILayout.Toggle ("Gravity", fo.gravityEnabled);
+		_showPhysicsAttrs = EditorGUILayout.Foldout(_showPhysicsAttrs, "Toggle Physics");
+		if (_showPhysicsAttrs) {
+			fo.liftEnabled = EditorGUILayout.Toggle ("Lift", fo.liftEnabled);
+			fo.dragEnabled = EditorGUILayout.Toggle ("Drag", fo.dragEnabled);
+			fo.gravityEnabled = EditorGUILayout.Toggle ("Gravity", fo.gravityEnabled);
+		}
 
-		fo.Unit = (UnitConverter.Units) EditorGUILayout.EnumPopup (fo.Unit);
-		fo.Preset = (FlightObject.Presets)EditorGUILayout.EnumPopup (fo.Preset);
-		fo.WingSpan = EditorGUILayout.FloatField ("Wing Span (" + fo.getLengthType() + ")", fo.WingSpan);
-		fo.WingChord = EditorGUILayout.FloatField ("Wing Chord (" + fo.getLengthType() + ")", fo.WingChord);
-		fo.WingArea = EditorGUILayout.FloatField ("Wing Area (" + fo.getAreaType() + ")", fo.WingArea);
-		fo.AspectRatio = EditorGUILayout.Slider ("Aspect Ratio", fo.AspectRatio, 1, 16);
-		fo.Weight = EditorGUILayout.FloatField ("Weight (" + fo.getWeightType() + ")", fo.Weight);
-		if( GUILayout.Button("Align To Wing Dimensions" ) ) 		{fo.setFromWingDimensions ();}
-		if (GUILayout.Button ("Align Dimensions From Area & AR") ) 	{fo.setWingDimensions ();}
+		_showDimensionAttrs = EditorGUILayout.Foldout (_showDimensionAttrs, "Wing Dimensions");
+		if (_showDimensionAttrs) {
+			fo.Unit = (UnitConverter.Units) EditorGUILayout.EnumPopup (fo.Unit);
+			fo.Preset = (FlightObject.Presets)EditorGUILayout.EnumPopup (fo.Preset);
+			fo.WingSpan = EditorGUILayout.FloatField ("Wing Span (" + fo.getLengthType() + ")", fo.WingSpan);
+			fo.WingChord = EditorGUILayout.FloatField ("Wing Chord (" + fo.getLengthType() + ")", fo.WingChord);
+			fo.WingArea = EditorGUILayout.FloatField ("Wing Area (" + fo.getAreaType() + ")", fo.WingArea);
+			fo.AspectRatio = EditorGUILayout.Slider ("Aspect Ratio", fo.AspectRatio, 1, 16);
+			fo.Weight = EditorGUILayout.FloatField ("Weight (" + fo.getWeightType() + ")", fo.Weight);
+			if( GUILayout.Button("Align To Wing Dimensions" ) ) 		{fo.setFromWingDimensions ();}
+			if (GUILayout.Button ("Align Dimensions From Area & AR") ) 	{fo.setWingDimensions ();}
+		}
 
-		ff.flightController = go.GetComponent<BaseFlightController> ();
-		EditorGUILayout.ObjectField ("Flight Controller", ff.flightController, typeof(MonoBehaviour), false);
-		tempgc = (MonoScript) EditorGUILayout.ObjectField ("Ground Controller", tempgc, typeof(MonoScript), false);
-		ff.GroundMode = EditorGUILayout.Toggle ("Ground Controller Enabled", ff.GroundMode);
-		//		EditorGUILayout.ObjectField ("Ground Controller", groundController, typeof(MonoBehaviour), false);
+		_showControllers = EditorGUILayout.Foldout (_showControllers, "Controllers");
+		if (_showControllers) {
+			ff.flightController = go.GetComponent<BaseFlightController> ();
+			EditorGUILayout.ObjectField ("Flight Controller", ff.flightController, typeof(BaseFlightController), false);
+			ff.Mode = (FreeFlight.Modes) EditorGUILayout.EnumPopup ("Flight Mode", ff.Mode);
+			
+
+			if (ff.groundController)
+				ff.groundController = (MonoBehaviour) EditorGUILayout.ObjectField("Ground Controller", ff.groundController, typeof(MonoBehaviour), false);
+			else
+				tempgc = (MonoScript) EditorGUILayout.ObjectField ("Ground Controller", tempgc, typeof(MonoScript), false);
+			//		EditorGUILayout.ObjectField ("Ground Controller", groundController, typeof(MonoBehaviour), false);
+		}
+
 
 		if (cname == null && tempgc)
 			cname = tempgc.name;
@@ -50,6 +70,9 @@ public class FreeFlightEditor : Editor {
 
 		groundController = (MonoBehaviour) go.GetComponent (cname);
 		if (groundController) {
+			if (ff.groundController && ff.groundController.name != groundController.name) {
+				Destroy (ff.groundController);
+			}
 			ff.groundController = groundController;
 		}
 
