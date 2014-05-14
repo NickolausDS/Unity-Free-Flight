178f0093cac49a43fe6fb7666b55de38d59a9212
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-14 09:11:12 -0700

    Added "Flight Mechanics"
    Flight Mechanics will handle the math for applying maneuvors
    to physics. It will keep the base flight controller from getting
    involved in physics, as it shouldn't.

---
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/FlightMechanics.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Flight Mechanics is responsible for taking user input and 
+ * manipulating the wing for various physics effects.
+ * 
+ * It handles the math behind flapping, flarring, diving,
+ * to change how physics effects the flight object.
+ * 
+ * Example: In order to do a 'dive', this class reduces the
+ * wing area. flight physics then calculates smaller lift forces,
+ * allowing the flight object to plummet.
+ */ 
+
+public class FlightMechanics : FlightPhysics {
+
+	public FlightMechanics(Rigidbody rb) : base(rb) {
+
+	}
+
+
+	public void wingFlare() {
+		return;
+	}
+
+	public void wingDive() {
+		return;
+	}
+
+	public void thrust() {
+		return;
+	}
+
+
+	//Get (in degrees) angle of attack for zero lift coefficient
+	private float getAOAForZeroLCOF() {
+		return 0.0f;
+	}
+
+
+
+
+}
