e118b146a10e1ab9eb8293f3e570d358c32a4a10
Nickolaus D. Saint <nick@incendiarysoftware.com>
2014-06-03 10:23:56 -0700

    Added ground control info to player HUD

--- Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
@@ -5,10 +5,12 @@ public class InfoMenu : MonoBehaviour {
 
 	void OnGUI() {
 		
-		string message = "'Esc' to pause.";
+		string message = "'Esc' to pause.\n" +
+			"'W A S D' to move.\n" +
+			"Double Jump to fly.";
 		
-		GUI.BeginGroup (new Rect (Screen.width - 130, 10, 200, 40));
-		GUI.Box (new Rect (0, 0, 120, 25), message);
+		GUI.BeginGroup (new Rect (Screen.width - 130, 10, 200, 90));
+		GUI.Box (new Rect (0, 0, 120, 60), message);
 		GUI.EndGroup ();
 	}
 }
