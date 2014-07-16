13b58f9ce3de89620f1281ff9c5bf6191700877a
Nickolaus D. Saint <nick@incendiarysoftware.com>
2014-07-16 10:37:21 -0700

    Added first person flight camera controller
    This is intended to handle transforms and rotations for the camera
    for when the player executes various flight mechanics

---
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/controllers/FPFlightCameraController.cs
@@ -0,0 +1,62 @@
+﻿/// <summary>
+/// Controls the camera in first person flight.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class FPFlightCameraController : MonoBehaviour {
+
+	public Transform cameraTransform;
+	public BaseFlightController flightController;
+	public float cameraSpeed = 3.0f;
+
+	private Quaternion savedRotation;
+
+	private bool alignedWithGameObject;
+	// Use this for initialization
+	void Awake () {
+		if (!cameraTransform && Camera.main)
+			cameraTransform = Camera.main.transform;
+		if (!cameraTransform) {
+			Debug.Log ("Please add a camera to FPFlightCameraController");
+			enabled = false;
+		}
+		if (!flightController)
+			flightController = gameObject.GetComponent<BaseFlightController>();
+		if (!flightController) {
+			Debug.Log ("Object does not have a flight controller. Please add one for FPFlightCameraController to work");
+			enabled = false;
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (flightController.flightEnabled) {
+
+			if(flightController.DoFlare) {
+				cameraTransform.rotation = savedRotation;
+				alignedWithGameObject = false;
+				//cameraTransform.rotation *= Quaternion.LookRotation( new Vector3 ( 0, -flightController.FlareAngle, 0) );
+			} else {
+				if (!alignedWithGameObject)
+					alignWithGameObject();
+				cameraTransform.rotation = Quaternion.Lerp (cameraTransform.rotation, gameObject.transform.rotation, cameraSpeed * Time.deltaTime );
+				savedRotation = cameraTransform.rotation;
+			}
+		} else {
+			if (!alignedWithGameObject)
+				alignWithGameObject();
+		}
+	
+	}
+
+	void alignWithGameObject() {
+		cameraTransform.rotation = Quaternion.Lerp (cameraTransform.rotation, gameObject.transform.rotation, cameraSpeed * Time.deltaTime );
+		if (Quaternion.Angle (cameraTransform.rotation, gameObject.transform.rotation) < 0.5f)
+			alignedWithGameObject = true;
+	}
+}
