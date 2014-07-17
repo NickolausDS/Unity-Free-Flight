b42adac3bfdececf94cba191e125d491d39177d5
Nickolaus D. Saint <nick@incendiarysoftware.com>
2014-07-17 10:11:10 -0700

    Scene update!
    * Added flare to input controls
    * Changed info-menu to reflect new launch method
    * Switched flight controller from mouse to keyboard
    * Added all peripheral ground controller scripts to free flight
    * Organized the script order a bit better in the editor

--- Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
@@ -7,7 +7,7 @@ public class InfoMenu : MonoBehaviour {
 		
 		string message = "'Esc' to pause.\n" +
 			"'W A S D' to move.\n" +
-			"Double Jump to fly.";
+			"Hold Spacebar to fly.";
 		
 		GUI.BeginGroup (new Rect (Screen.width - 130, 10, 200, 90));
 		GUI.Box (new Rect (0, 0, 120, 60), message);
