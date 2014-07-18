889c81fb87c60cfef9efb6e15d120dc9ae6c388f
Nickolaus D. Saint <nick@incendiarysoftware.com>
2014-07-18 09:29:01 -0700

    Updated flight controls in info menu

--- Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/demo/Scripts/InfoMenu.cs
@@ -7,10 +7,14 @@ public class InfoMenu : MonoBehaviour {
 		
 		string message = "'Esc' to pause.\n" +
 			"'W A S D' to move.\n" +
-			"Hold Spacebar to fly.";
+			"Hold Spacebar to fly.\n\n" +
+			"In flight:\n" +
+			"Spacebar to flap.\n" +
+			"'Q and E' to dive.\n" +
+			"Left Shift to flare.";
 		
-		GUI.BeginGroup (new Rect (Screen.width - 130, 10, 200, 90));
-		GUI.Box (new Rect (0, 0, 120, 60), message);
+		GUI.BeginGroup (new Rect (Screen.width - 140, 10, 200, 200));
+		GUI.Box (new Rect (0, 0, 130, 130), message);
 		GUI.EndGroup ();
 	}
 }
