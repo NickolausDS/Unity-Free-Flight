da1e13389b2f60989555fdf645c7187e7d8088be
Nickolaus D. Saint <nick@incendiarysoftware.com>
2014-07-27 11:59:12 -0700

    Added basic trigger system
    The trigger system waits for the user to do things, then 
    executes actions based on what they do. This will be used
    for an interactive tutorial to start.

---
+++ Unity Free Flight/Assets/Triggers/BaseTrigger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseTrigger : MonoBehaviour {
+
+	public BaseTrigger chainToTrigger;
+	public float delaySecondsToAction = .75f;
+	public float delaySecsAfterAction = 2.0f; 
+
+	private bool conditionSatisfied = false;
+	private float conditionSatisfiedTime;
+
+	private bool actionFired = false;
+	private float actionSatisfiedTime;
+
+	// Use this for initialization
+	void Start () {
+	
+		initialAction ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (condition ()) {
+			conditionSatisfied = true;
+			conditionSatisfiedTime = Time.time;
+		}
+
+		if (conditionSatisfied && (Time.time > conditionSatisfiedTime + delaySecondsToAction)) {
+			action ();
+
+			actionFired = true;
+			actionSatisfiedTime = Time.time;
+
+			//We don't want to fire the action twice
+			conditionSatisfied = false;
+		}
+
+		if (actionFired && (Time.time > actionSatisfiedTime + delaySecsAfterAction) ) {
+			if (chainToTrigger)
+				chainToTrigger.enabled = true;
+			this.enabled = false;
+		}
+	
+	}
+
+	protected abstract void initialAction ();
+
+	protected abstract bool condition ();
+
+	protected abstract void action ();
+
+}
---
+++ Unity Free Flight/Assets/Triggers/KeyPressTextBlurb.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPressTextBlurb : BaseTrigger {
+	
+	public string textLine1;
+	public string textLine2;
+	public string textLine3;
+	public string button;
+	public Vector4 textPosition = new Vector4(-300, 10, 200, 60);
+	public bool relativeToScreenWidth = true;
+	public bool relativeToScreenHeight = false;
+
+	private bool showGUI = true;
+
+	protected override void initialAction () {}
+
+	protected override bool condition ()
+	{
+		if (Input.GetButton (button) )
+			return true;
+		return false;
+	}
+
+	protected override void action () {
+		showGUI = false;
+	}
+
+	void OnGUI() {
+
+		if (showGUI) {
+
+			Vector4 actPos = textPosition;
+
+			if (relativeToScreenWidth)
+				actPos.x += Screen.width;
+			if (relativeToScreenHeight)
+				actPos.y += Screen.height;
+
+			GUI.Box (new Rect (actPos.x, actPos.y, actPos.z, actPos.w), textLine1 + '\n' + textLine2 + '\n' + textLine3);
+		}
+	}
+	
+}
