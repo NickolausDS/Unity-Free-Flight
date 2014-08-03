4939fcd1b18506cb3387d606c4ebe146d82f3375
Nickolaus D. Saint <nick@incendiarysoftware.com>
2014-08-03 13:14:50 -0700

    Improved dive mechanic
    * Symmetrical dives now rotate the player towards the ground
    * Rotation is based on velocity
    Note: A stability issue still exists with asymmetrical dives.

--- Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/FlightMechanics.cs
+++ Unity Free Flight/Assets/UnityFreeFlight/scripts/physics/FlightMechanics.cs
@@ -127,8 +127,19 @@ public class FlightMechanics : FlightPhysics {
 
 	public void wingFold(float left, float right) {
 		setWingPosition (left, right);
-		rigidbody.AddTorque (rigidbody.rotation * Vector3.forward * (right - left) * 2.0f);
+
+		float torqueSpeed = (rigidbody.velocity.magnitude) / 15.0f;
+		rigidbody.AddTorque (rigidbody.rotation * Vector3.forward * (right - left) * torqueSpeed);
 		rigidbody.angularDrag = left * right * 3.0f;
+
+		if (!wingsOpen()) {
+			//Rotate the pitch down based on the angle of attack
+			//This gives the player the feeling of falling
+			Quaternion pitchRot = Quaternion.identity;
+			pitchRot.eulerAngles = new Vector3 (AngleOfAttack, 0, 0);
+			pitchRot = rigidbody.rotation * pitchRot;
+			rigidbody.rotation = Quaternion.Lerp (rigidbody.rotation, pitchRot, torqueSpeed * Time.deltaTime);
+		}
 
 	}
 
