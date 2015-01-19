88a3d5127346ecb273d2b5b6ba501f4c79da44ce
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-01-19 11:44:11 -0800

    Made MenuResizer script work in scene view

--- Unity Free Flight/Assets/FreeFlight/Scripts/demo/MenuResizer.cs
+++ Unity Free Flight/Assets/FreeFlight/Scripts/demo/MenuResizer.cs
@@ -3,29 +3,43 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+[ExecuteInEditMode]
 public class MenuResizer : MonoBehaviour {
 
-	public RectTransform resizableCanvas;
+	public GameObject menu;
+	public bool autoSetMenuItems = true;
+	private RectTransform menuRectTransform;
 	public List<GameObject> items;
 	public int paddingPerItem = 20;
 	public int perItemSize = 70;
 
-	void Start() {
-		resizableCanvas = gameObject.GetComponent<RectTransform> ();
-		foreach (Transform child in transform) {
-			items.Add (child.gameObject);
+	void OnRenderObject() {
+
+		if (autoSetMenuItems && menu != null) {
+			resetMenuItems();
 		}
 
+		if (items.Count > 0) {
+			resizeMenu();
+		}    
 	}
 
-	// Update is called once per frame
-	void Update () {
+	private void resetMenuItems() {
+		menuRectTransform = menu.GetComponent<RectTransform> ();
+		items.Clear ();
+		foreach (Transform child in menu.transform) {
+				items.Add (child.gameObject);
+		}
+	}
+
+	private void resizeMenu() {
 		int activeItems = 0;
 		foreach (GameObject item in items) {
 			if (item.activeInHierarchy)
 				activeItems++;
 		}
 		int totalHeight = perItemSize * activeItems + (activeItems * 2 - 1) * paddingPerItem;
-		resizableCanvas.sizeDelta = new Vector2 (resizableCanvas.sizeDelta.x, totalHeight);
+		menuRectTransform.sizeDelta = new Vector2 (menuRectTransform.sizeDelta.x, totalHeight);	
 	}
+	
 }
