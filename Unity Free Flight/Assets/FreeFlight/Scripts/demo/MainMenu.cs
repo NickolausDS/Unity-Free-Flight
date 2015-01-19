14d9d5da998b751189bde72443ac43d4f951f454
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-01-19 11:04:24 -0800

    Added main menu
    The addition of the main menu does a couple things: 
    * User experience is better. The game no longer drops them into
    play when they aren't ready, they choose when to start. 
    * Perspective performance is better. Scenes are loaded independently,
    meaning the main menu loads very fast, and the player can click around
    while the main game slowly loads. Giving the player something to do while
    loading happens improves the perspective loading time.

---
+++ Unity Free Flight/Assets/FreeFlight/Scripts/demo/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace FreeFlightDemo {
+
+	public class MainMenu : MonoBehaviour {
+
+		public GameObject playButton;
+		public GameObject exitButton;
+		public GameObject loadLevelOutputText;
+
+		public bool disableWebPlayButtonUntilLevelLoad = true;
+		public bool disableWebExitButton = true;
+
+		public const int defaultLevel = 1;
+
+		public void Start() {
+			if (disableWebPlayButtonUntilLevelLoad && Application.isWebPlayer)
+				playButton.GetComponent<Button>().interactable = false;
+			if (disableWebExitButton && (Application.isEditor || Application.isWebPlayer) && exitButton)
+				exitButton.SetActive (false);
+		}
+
+		public void Update() {
+
+			if (disableWebPlayButtonUntilLevelLoad && Application.isWebPlayer && loadLevelOutputText)
+				updateLoadStatus ();
+		}
+
+		public void ButtonStart(int level = defaultLevel) {
+			Application.LoadLevel (level);
+		}
+
+		public void ButtonQuit() {
+			Application.Quit ();
+		}
+
+		private void updateLoadStatus() {
+			float progress = Application.GetStreamProgressForLevel (defaultLevel);
+			
+			if (loadLevelOutputText) {
+				if (progress == 1f)
+					loadLevelOutputText.GetComponent<Text>().text = "";
+				else 
+					loadLevelOutputText.GetComponent<Text>().text = string.Format ("Level loaded: {0:P0}", progress);
+			}
+			
+			if (playButton) {
+				if (progress < 1f)
+					playButton.GetComponent<Button>().interactable = false;
+				else
+					playButton.GetComponent<Button>().interactable = true;
+			}
+		}
+	}
+}
---
+++ Unity Free Flight/Assets/FreeFlight/Scripts/demo/MenuResizer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuResizer : MonoBehaviour {
+
+	public RectTransform resizableCanvas;
+	public List<GameObject> items;
+	public int paddingPerItem = 20;
+	public int perItemSize = 70;
+
+	void Start() {
+		resizableCanvas = gameObject.GetComponent<RectTransform> ();
+		foreach (Transform child in transform) {
+			items.Add (child.gameObject);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int activeItems = 0;
+		foreach (GameObject item in items) {
+			if (item.activeInHierarchy)
+				activeItems++;
+		}
+		int totalHeight = perItemSize * activeItems + (activeItems * 2 - 1) * paddingPerItem;
+		resizableCanvas.sizeDelta = new Vector2 (resizableCanvas.sizeDelta.x, totalHeight);
+	}
+}
