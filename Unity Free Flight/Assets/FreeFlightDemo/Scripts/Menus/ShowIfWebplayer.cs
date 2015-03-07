d657365ef5c85c41d3cecd26e6faafb37fbd56c6
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-03-07 09:33:43 -0800

    Added fullscreen message for webplayer
    When the player starts the game inside a webplayer, they
    get a "you should make this fullscreen" message. Fullscreen
    grants the player more performance, so this is preferable.

---
+++ Unity Free Flight/Assets/FreeFlightDemo/Scripts/Menus/ShowIfWebplayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowIfWebplayer : MonoBehaviour {
+
+	[Tooltip ("Force show object even if this isn't running inside the webplayer")]
+	public bool showForTesting = false;
+	// Use this for initialization
+	void Start () {
+		if (!Application.isWebPlayer && !showForTesting)
+			gameObject.SetActive (false);
+	}
+
+}
