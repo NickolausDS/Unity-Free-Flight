fb44678a6ab91d6347fe562aef380585e8cb524b
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-03-22 16:02:57 -0700

    Refactored the stats viewer to be more generic an reusable
    The StatsObject is now responsible for the UI stuff, while the override class tells it 
    what to display. StatsObject will be used for more than just viewing inputs in the future,
    probably including:
    
    * Physics statistics
    * Mode statistics
    * Current Mechanic statistics

---
+++ Unity Free Flight/Assets/FreeFlight/Scripts/Mods/FlightInputStats.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityFreeFlight;
+
+namespace UnityFreeFlight {
+
+	public class FlightInputStats : StatsObject {
+
+		public GameObject flightObject;
+		private FreeFlight ffComponent;
+		private FlightInputs fInputs;
+
+		public override void Start () {
+			base.Start ();
+			nullCheck ("flightObject", flightObject, "Please set it to an object with a Free Flight Component");
+		}
+		
+		public void Update () {
+			updateText (fInputs, flightObject.name);
+		}
+
+		public override void autoConfig() {
+			base.autoConfig ();
+
+			if (!flightObject) {
+				flightObject = GameObject.FindGameObjectWithTag ("Player");
+			}
+
+			if (!ffComponent) {
+				ffComponent = flightObject.GetComponent<FreeFlight> ();
+			}
+
+			if (fInputs == null && ffComponent != null)
+				fInputs = ffComponent.modeManager.flightMode.flightInputs;
+
+		}
+
+	}
+
+}
---
+++ Unity Free Flight/Assets/FreeFlight/Scripts/Mods/StatsObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Reflection;
+using UnityFreeFlight;
+
+namespace UnityFreeFlight {
+
+public class StatsObject : MonoBehaviour {
+	
+		public Text text;
+		
+		// Use this for initialization
+		public virtual void Start () {
+			autoConfig ();
+			nullCheck ("text", text, gameObject.name + " missing a 'Text' UI object to display stats information");
+			
+		}
+		
+		public void updateText (System.Object obj, string headerTitle) {
+			string newText = headerTitle + ":\n";
+			BindingFlags flags = /*BindingFlags.NonPublic | */BindingFlags.Public | 
+				BindingFlags.Instance | BindingFlags.Static;
+			PropertyInfo[] flightInputProperties = obj.GetType().GetProperties(flags);
+			foreach (PropertyInfo propertyInfo in flightInputProperties) {
+				newText += string.Format ("{0}: {1}\n", propertyInfo.Name, propertyInfo.GetValue(obj, null));
+			}
+			text.text = newText;
+		}
+		
+		public virtual void autoConfig() {
+			if (text == null)
+				text = GetComponentInChildren<Text> ();
+		}
+		
+		public void nullCheck (string name, System.Object obj, string helpMSG) {
+			if (obj == null || obj.ToString().Equals ("null")) {
+				Debug.LogError(this.name + ": " + name + " is null. " + helpMSG);
+				this.enabled = false;
+			}
+
+		}
+		
+		
+	}
+}
