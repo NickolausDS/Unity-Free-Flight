3fe0a4a0310de32d5cb59684752ed9abe3768d4f
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-04-02 07:48:20 -0700

    Added testing files which perfectly replicate rigidbody behaviour
    * These won't ever be directly useful to the project, but they will be invaluable for
    demonstrating how to apply physics with or without rigidbodies in a scene.

---
+++ Unity Free Flight/Assets/FreeFlight/Scripts/FreeFlightPhysics/PhysicsTools/DropTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Physics testing for how a rigidbody moves an object over 
+/// a duration of time and physics "ticks" (Fixed update calls)
+/// </summary>
+public class DropTest : MonoBehaviour {
+
+	public float collisionTime = 0f;
+	public bool collisionTimer = true;
+	public Vector3 gravity = Physics.gravity;
+	public float fixedUpdateTimeScale = .02f;
+	public Vector3 antiForce = Vector3.zero;
+	// Use this for initialization
+	Rigidbody rb;
+	private int startTime = -1;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		Physics.gravity = gravity;
+		Time.fixedDeltaTime = fixedUpdateTimeScale;
+	}
+
+	void FixedUpdate() {
+//		if (startTime == 0f) {
+//			startTime = Time.realtimeSinceStartup;
+//			Debug.Log ("Start time set at " + startTime);
+//		}
+		startTime++;
+
+		Debug.Log (string.Format (
+			"Fixed Update Ticks: {0}\n" +
+			"Position: {1}\n" +
+			"Velocity: {2}\n", startTime, rb.position, rb.velocity.magnitude));
+
+		rb.AddForce (antiForce, ForceMode.Force);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col) {
+		collisionTime = Time.realtimeSinceStartup;
+	}
+}
---
+++ Unity Free Flight/Assets/FreeFlight/Scripts/FreeFlightPhysics/PhysicsTools/TransformDropTest.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This component is a reimplementation of rigidbody force and drag physics. 
+/// It's useful for replicating rigidbody behaviour without using them. 
+/// </summary>
+public class TransformDropTest : MonoBehaviour {
+	
+	public float mass = 1f;
+	public float drag;
+	public Vector3 acceleration = Vector3.zero;
+	public Vector3 gravity = Physics.gravity;
+
+	private int fixedUpdateTicks = -1;
+	public bool atRest = false;
+	
+	void FixedUpdate() {
+
+		//Stop moving if we're at rest. 
+		if (atRest)
+			return;
+
+		//Log debugging information
+		fixedUpdateTicks++;
+		Debug.Log (string.Format (
+			"Fixed Update Ticks: {0}\n" +
+			"Position: {1}\n" +
+			//To derive velocity from acceleration, we need to divide by time
+			"Velocity: {2}\n", fixedUpdateTicks, transform.position, acceleration.magnitude/Time.fixedDeltaTime));
+
+		//Add the force
+		AddForce (gravity, ForceMode.Acceleration);
+
+		//Apply Linear Damping (drag)
+		ApplyDrag ();
+
+		//Move the object
+		transform.position += acceleration;
+
+	}
+
+	void OnTriggerEnter() {
+		atRest = true;
+	}
+
+	public void AddForce(Vector3 force, ForceMode forceType) {
+		switch (forceType) {
+		case ForceMode.Force:
+			//Force mode moves an object according to it's mass
+			acceleration = acceleration + force * mass * Time.fixedDeltaTime * Time.fixedDeltaTime;
+			break;
+		case ForceMode.Acceleration:
+			//Acceleration ignores mass
+			acceleration = acceleration + force * Time.fixedDeltaTime * Time.fixedDeltaTime;
+			break;
+		default:
+			throw new UnityException("Force mode not supported!");
+		}
+	}
+
+	//Apply Linear Damping
+	public void ApplyDrag() {
+		acceleration = acceleration * (1 - Time.deltaTime * drag);
+	}
+}
