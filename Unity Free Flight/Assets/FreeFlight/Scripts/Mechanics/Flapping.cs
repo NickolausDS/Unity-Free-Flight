b268c7216483cb1ece93ec9a0284951e29cff322
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-04-26 11:21:02 -0700

    Refactored Flapping
    * Fixed flapping sound issues
    * Fixed flapping animation issues
    * Flapping now has functionality to specify the force/direction vector of flap

--- Unity Free Flight/Assets/FreeFlight/Scripts/Mechanics/Flapping.cs
+++ Unity Free Flight/Assets/FreeFlight/Scripts/Mechanics/Flapping.cs
@@ -9,19 +9,35 @@ namespace UnityFreeFlight {
 	[Serializable]
 	public class Flapping : Mechanic {
 
-		[Header("Settings")]
-		public string flappingAnimation = "Flapping";
-		private int flappingHash;
-		public float flapStrength = 20.0f;
-		AnimatorStateInfo curstate;
+		[Header("Animation")]
+		public string flappingParameter = "Flapping";
+		public string flappingState = "Base Layer.Flapping";
+		private int paramHash;
+		private int stateHash;
+
+		[Header("Sound")]
 		public AudioClip[] sounds;
 		public SoundManager soundManager = new SoundManager();
+
+		[Header("General")]
+		[Tooltip("Units/second of force applied to object")]
+		public float flapStrength = 5.0f;
+		[Tooltip("Time in seconds before any flap force is applied")]
+		public float lagTime = 0f;
+		[Tooltip("Duration in seconds of flap (not connected to animation length)")]
+		public float duration = .5f;
+		private float startTime;
 
 		public override void init (GameObject go, FlightPhysics fp, FlightInputs fi) {
 			base.init (go, fp, fi);
 			soundManager.init (go);
 			name = "Flapping Mechanic";
-			setupAnimation (flappingAnimation, ref flappingHash);
+			setupAnimation (flappingParameter, ref paramHash);
+			stateHash = Animator.StringToHash (flappingState);
+			if (!animator.HasState (0, stateHash)) {
+				Debug.LogWarning ("Flapping: Animation Controller doesn't appear to have the '" + flappingState + "' state.");
+			}
+			
 		}
 
 		public override bool FFInputSatisfied () {
@@ -34,26 +50,45 @@ namespace UnityFreeFlight {
 		public override void FFStart () {}
 
 		public override void FFFixedUpdate () {
-			if (!isFlapping()) {
-				soundManager.playRandomSound(sounds);
-				rigidbody.AddForce (rigidbody.rotation * Vector3.up * flapStrength);
-				animator.SetTrigger (flappingHash);
-			}
+//			Debug.Log (curstate.shortNameHash + " : " + Animator.StringToHash (flappingAnimation));
+			if (animator.GetNextAnimatorStateInfo(0).fullPathHash != stateHash &&
+			    animator.GetCurrentAnimatorStateInfo(0).fullPathHash != stateHash &&
+			    FFInputSatisfied()
+			    ) {
+					soundManager.playRandomSound(sounds);
+					animator.SetTrigger (paramHash);
+					startTime = Time.time;
+			} 
+
+			applyFlapForce ();
 		}
 
 		/// <summary>
 		/// Since flapping animation is done on a trigger, we want to override the default behavior.
 		/// </summary>
 		public override bool FFFinish () {
-			return true;
+			return isDoneFlapping();
 		}
 
-		public bool isFlapping () {
-			curstate = animator.GetCurrentAnimatorStateInfo (0);
-			if (curstate.fullPathHash != flappingHash) {
-				return false;
+		public bool isDoneFlapping() {
+			if (startTime + duration + lagTime < Time.time)
+				return true;
+			return false;
+		}
+
+		public void applyFlapForce() {
+			if (startTime + lagTime < Time.time && startTime + duration + lagTime > Time.time) {
+				rigidbody.AddForce (getFlapForce (0f));
 			}
-			return true;
+		}
+
+		/// <summary>
+		/// Get the force vector for the flapping force (including direction and strength of flap)
+		/// </summary>
+		/// <returns>The flap force.</returns>
+		/// <param name="angle">direction of flap. 0 = thrusts forward, 90 = thrusts stright up.</param>
+		public Vector3 getFlapForce(float angle) {
+			return rigidbody.rotation * Quaternion.AngleAxis (angle, Vector3.right) * Vector3.forward * flapStrength;
 		}
 
 
