7e5df5ea40e3582bb3bf6704023f08471fc9d602
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-08-03 14:10:00 -0700

    Added gamepad support for pause menu on Mac
    Not tested on Windows or Linux.

--- Unity Free Flight/Assets/FreeFlightDemo/Scripts/PauseMenu.cs
+++ Unity Free Flight/Assets/FreeFlightDemo/Scripts/PauseMenu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -14,6 +15,18 @@ namespace FreeFlightDemo {
 		[Tooltip ("The Canvas that gets set to active on a pause event")]
 		public GameObject mainMenu;
 
+		[Tooltip ("GameObject with Event System Component. Used for setting selected object.")]
+		public GameObject eventSystem;
+		private EventSystem eventSystemComponent { 
+			get { 
+				if (__eventSystemComponent == null)
+					__eventSystemComponent = eventSystem.GetComponent<EventSystem>();
+				return __eventSystemComponent;
+			}
+		}
+		private EventSystem __eventSystemComponent;
+		private Selectable selectable;
+
 		private GameObject activeMenu;
 		public string[] availableLevels = {"MainMenu", "Grounded"};
 
@@ -24,40 +37,78 @@ namespace FreeFlightDemo {
 			activeMenu = mainMenu;
 			if (isPaused)
 				pause ();
-		}
-		
-		// Update is called once per frame
-		void Update () {
-			if (Input.GetKeyDown(KeyCode.Escape)) {
-				if (isPaused) {
-					if (activeMenu == mainMenu)
-						unpause ();
-					else 
-						selectMenu(mainMenu);
-				}
-				else
-					pause ();
+
+			if (!eventSystem) {
+				eventSystem = GameObject.Find ("EventSystem");
+				if (!eventSystem)
+					throw new UnityException (gameObject.name + ": Please set the 'Event System' object.");
 			}
-		
+
+			selectable = gameObject.GetComponent<Selectable> ();
+			if (!selectable)
+				Debug.LogWarning ("No 'Selectable' component on " + gameObject.name + ". Selecting things on" +
+				                  " this menu may not work for all devices.");
 		}
 
 		public void pause() {
 			Time.timeScale = 0.0f;
 			isPaused = true;
+			//Disallow the pause menu from stealing interaction from menu items
+			selectable.interactable = false;
 			selectMenu (mainMenu);
 		}
 
 		public void unpause() {
+			selectable.interactable = true;
+			eventSystemComponent.UpdateModules ();
+			eventSystemComponent.SetSelectedGameObject (gameObject);
+
 			Time.timeScale = 1.0f;
 			isPaused = false;
 			mainMenu.SetActive (false);
 		}
 
+		public void togglePause() {
+			if (!isPaused) {
+				if (activeMenu == mainMenu) {
+					pause ();
+				} else {
+					selectMenu(mainMenu);
+				}
+			} else {
+				unpause();
+			}
+		}
+
 		public void selectMenu(GameObject newMenu) {
+			//Enable game object for the menu
 			if (activeMenu != null)
 				activeMenu.SetActive (false);
 			activeMenu = newMenu;
-			activeMenu.SetActive (true);       
+			activeMenu.SetActive (true);
+
+			//Search the new menu for the first 'Selectable' object and transfer
+			//selection to it. We need at least one selectable object to point us
+			//back to the main menu, or our selection is lost in the woods. 
+			//(small hack: by counting from the highest child to zero, we start at
+			//the top of the menu. I'm not sure why this is the case. It may be wise
+			//to have a better definition for the "topmost" menu item in the future
+			//rather than depend on this hack.) 
+			for (int i = newMenu.transform.childCount - 1; i >= 0; --i) {
+				Transform child = newMenu.transform.GetChild(i);
+				Selectable sel = child.gameObject.GetComponent<Selectable>();
+				if (sel != null && sel.interactable == true) {
+					eventSystemComponent.SetSelectedGameObject (child.gameObject);
+					eventSystemComponent.UpdateModules ();
+				}			
+			}
+
+
+			if (!eventSystemComponent.currentSelectedGameObject.transform.IsChildOf(newMenu.transform))
+				Debug.LogWarning ("Could not transfer selection to menu " + newMenu + ". Make sure" +
+				                  " it has child menu objects with 'Selectable' components that " +
+				                  " are interactable.");
+
 		}
 
 		public void loadLevel(int level) {
