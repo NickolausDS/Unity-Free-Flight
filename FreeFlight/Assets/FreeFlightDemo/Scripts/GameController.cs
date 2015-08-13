e25aa9979d5e4fe9b1e74b8b47745b8814b3d1e8
Nickolaus D. Saint <nick@incendiarysoftware.com>
2015-08-12 22:03:33 -0400

    Scene update. Integrated Oculus optional use by menu
    * Oculus is now disabled by default
    * Oculus can be enabled in options menu
    * Updates to scene and prefabs

---
+++ FreeFlight/Assets/FreeFlightDemo/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameController : MonoBehaviour {
+
+	[Tooltip ("Normal Main Camera. Enabled by default.")]
+	public GameObject normalCamera;
+	[Tooltip ("Oculus Camera. Not enabled by default.")]
+	public GameObject oculusCamera;
+	[Tooltip ("(Optional) Needed for setting camera render on camera switch.")]
+	public GameObject menu;
+	[Tooltip ("(Optional) Needed for seting camera render on camera switch.")]
+	public GameObject hud;
+	private bool oculusEnabled;
+
+	public void toggleOculus(bool value) {
+		oculusEnabled = value;
+		normalCamera.SetActive (!oculusEnabled);
+		oculusCamera.SetActive (oculusEnabled);
+
+		if (oculusEnabled) {
+			if (hud != null)
+				hud.GetComponent<Canvas> ().worldCamera = oculusCamera.GetComponentInChildren<Camera> ();
+			if (menu != null)
+				menu.GetComponent<Canvas> ().worldCamera = oculusCamera.GetComponentInChildren<Camera> ();
+		}
+	}
+
+}
